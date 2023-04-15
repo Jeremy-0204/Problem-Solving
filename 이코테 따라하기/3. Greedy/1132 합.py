c4c810036ba1a3289f4af678b795d56e9acf7d3b
Jeremy <parkkyoungrun@gmail.com>
2023-04-15 21:42:20 +0900

    Update 1132 합.py
    길이 1의 문자는 0이 되면 안되는 예외조건 처리 중

--- 이코테 따라하기/3. Greedy/1132 합.py
+++ 이코테 따라하기/3. Greedy/1132 합.py
@@ -5,6 +5,8 @@ for _ in range(n):
     array.append(str(input()))
 
 array = sorted(array, key = lambda word: len(word), reverse = True)
+print(array)
+
 alpha = sorted(set(''.join(array))) # 지금까지 들어있는 알파벳만 추출
 alpha = {a : 0 for a in alpha} # 알파벳 A~J, 가중치 dict 생성
 
@@ -17,7 +19,22 @@ for target in alpha.keys():
         count += sum(index)
     alpha[target] = count
 
+alpha = dict(sorted(alpha.items(), key = lambda x: x[1]))
+
 print(alpha)
+
+for i in alpha:
+    for word in array[::-1]:
+        if i[0] in word and len(word) == 1: 
+            alpha[i[0]] *= -1
+            break
+        elif i[0] in word and len(word) > 1: 
+            print(word, i[0])
+            continue
+ 
+print(alpha)
+
+
 
 
 # ABC
