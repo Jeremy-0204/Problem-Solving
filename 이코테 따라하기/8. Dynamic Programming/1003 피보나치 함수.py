a07bda1c66307288d64db2f5643f338d58448d26
Jeremy <parkkyoungrun@gmail.com>
2023-05-08 23:19:34 +0900

    Update 1003 피보나치 함수.py

--- 이코테 따라하기/8. Dynamic Programming/1003 피보나치 함수.py
+++ 이코테 따라하기/8. Dynamic Programming/1003 피보나치 함수.py
@@ -1,11 +1,14 @@
-dp = [0] * 40
+def fib(n):
+    zero = [1, 0, 1]
+    one = [0, 1, 1]
 
-dp[0], dp[1] = 0, 1
+    if n >= 3:
+        for i in range(2, n):
+            zero.append(zero[i-1] + zero[i])
+            one.append(one[i-1] + one[i])
+    print(f"{zero[n]} {one[n]}")
 
-
-for _ in range(int(input())):
+test = int(input())
+for _ in range(test):
     n = int(input())
-
-    print(fib(n))
-
-    
+    fib(n)
