120b6b3a9f9780b03d1cc1566bbd23bd6530a2b8
Jeremy <parkkyoungrun@gmail.com>
2023-07-09 22:50:35 +0900

    Create 5525 IOIOI.py

---
+++ 이코테 따라하기/그 외 문제/5525 IOIOI.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+s = input()
+
+cursor, count, result = 0, 0, 0
+while cursor < (m - 1):
+    if s[cursor : cursor + 3] == "IOI":
+        cursor += 2
+        count += 1
+
+        if count == n:
+            result += 1
+            count -= 1
+    
+    else:
+        cursor += 1
+        count = 0
+
+print(result)
